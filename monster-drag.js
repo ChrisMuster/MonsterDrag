ca67f270c60ca460bfae4db9eb78952cd1ec3a06
Chris Muster <christophermuster@yahoo.co.uk>
2017-09-20 15:37:51 +0100

    Create monster-drag.js

---
+++ monster-drag.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  //code to drag eyes around the screen, for fun
+	$("#eye1").draggable();
+	$("#eye5").draggable();
+	$("#scaryeyes").draggable();
+
+	$("#grin").draggable();
+
+	function grinny() {
+		$("#grin").fadeIn(2000).fadeOut(2000).fadeIn(2000);
+	}
+
+	$("#grin").click(function() {
+		grinny();
+	});
+});//end doc.onready function
